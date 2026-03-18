93315eb393d99fb8a01087ce3c1cdda2e95c6441
Yash <macmini@Yashs-Mac-mini.local>
2026-03-18 21:27:27 +0530

    feat: add util to break pdf text into chunks

--- src/controllers/content.controller.js
+++ src/controllers/content.controller.js
@@ -1,19 +1,26 @@
 import { prisma } from "../server.js";
-import { getEmbeddings } from "../utils/services/ai.service.js";
-import { cleanPdfText, validatePdfResult } from "../utils/services/pdf.service.js";
+import { getEmbeddings } from "../utils/services/ai.service.js";import { cleanPdfText, getPdfChunks, validatePdfResult } from "../utils/services/pdf.service.js";
 import { extractText } from "unpdf";
 
 export const uploadFile = async (req, res, next) => {
     const file = req.file;
-    const data = await extractText(new Uint8Array(file.buffer));
-    const pdfPages = cleanPdfText(data.text)
+
+    // get all text extracted from PDF as a string
+    const data = await extractText(new Uint8Array(file.buffer), { mergePages: true });
+
+    // clean text
+    const cleanText = cleanPdfText(data.text)
 
     // throws error if not satisfied with the conditions
-    validatePdfResult(pdfPages)
+    validatePdfResult(cleanText)
 
-    const embeddings = await getEmbeddings(pdfPages)
+    // get chunks 
+    // Note: (smaller chunks -> more api calls for embeddings + more rows are created + weaker context per chunk)
+
+    const chunks = getPdfChunks(cleanText, 20, 800)
+    
 
     res.json({
-        embeddings: embeddings
+        chunks
     })
 }
--- src/utils/services/pdf.service.js
+++ src/utils/services/pdf.service.js
@@ -1,26 +1,55 @@
 import opError from "../classes/opError.class.js"
 
 // removes encoded text like \n , \s
-export const cleanPdfText = (pdfPages = ['defaultText']) => {
-
-  return pdfPages.map(text => 
-    text
+export const cleanPdfText = (text = 'defaultText') => {
+    return text
       .replace(/\n+/g, ' ')
       .replace(/\s+/g, ' ')
       .trim()
-    )
 }
 
 // validates extracted text of PDF
-export const validatePdfResult = (pdfPages=[]) => {
+export const validatePdfResult = (pdfText = '') => {
 
   // if every page has no text
-  if (pdfPages.every(page => page === '')) {
+  if (pdfText === '') {
     throw new opError('No text found or invalid pdf', 400)
   }
 
-  if (pdfPages.length > 10) {
-    throw new opError('Pages must be less than 10', 400)
+}
+
+export const getPdfChunks = (text, overlap=20, maxChunkSize=800) => {
+
+  // excludes whitespaces
+  const words = text.split(/\s+/);
+  const chunks = []
+
+  let i = 0
+  let currentChunk = '';
+
+  while (i < words.length) {    
+    
+    if (currentChunk.length >= maxChunkSize) {
+
+      // get the prev chunk's last words to keep the context in current chunk
+      const overlapChunk = words.slice(i - overlap, i).join(' ')
+
+      // push chunk
+      if (currentChunk.trim()) chunks.push(currentChunk.trim())
+
+        // update with context words
+        currentChunk = overlapChunk
+
+      }
+
+      // keep grouping the text
+    currentChunk += ' ' + words[i]
+
+    i++
   }
 
-}
+  // push the remaining chunk
+  chunks.push(currentChunk.trim())
+
+  return chunks;
+};
